2a9e52fb59a594d09cf81ad9068a009b428896f2
Anushka Tiwari <codebyanushka@gmail.com>
2025-08-12 11:34:01 +0530

    Your commit message here

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -9,6 +9,7 @@ android {
     namespace = "com.example.xpensure_frontend"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+    ndkVersion="27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
